8ada30bb1c8658f8c808b81d72119da037448967
mhamada <m.hamada@ntt.com>
2018-10-19 12:03:39 +0900

    sampleにコメント入れた

--- example/example.py
+++ example/example.py
@@ -15,7 +15,12 @@ args = parser.parse_args()
  
 parser = Parser(args.client_id, args.client_secret, args.developer_api_base_url, args.access_token_publish_url)
 s = parser.parse(input())
-print(s.form)
-print(s.get_root_chunk().form)
-print(s.get_root_chunk().get_children(filter=["agent"])[0].get_chunk_head_token().form)
 
+# 入力文そのまま
+print(s.form)
+# 述語の文節の表記を取得
+print(s.get_predicate().form)
+# 述語の動作主を取得
+print(s.get_predicate().get_children(filter=["agent"])[0].get_chunk_head_token().form)
+# 述語の動作主を形容している語を取得
+print(s.get_predicate().get_children(filter=["agent"])[0].get_chunk_head_token().get_children(filter=["nomd"])[0].form)
