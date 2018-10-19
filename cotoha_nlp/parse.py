b1953291f844bd2ceeaa206ca98d2f944fd6947f
mhamada <m.hamada@ntt.com>
2018-10-19 12:03:03 +0900

    access tokenを直接指定して初期化できるようにした

--- cotoha_nlp/parse.py
+++ cotoha_nlp/parse.py
@@ -7,24 +7,30 @@ import json
 from .data.sentence import Sentence
 
 class Parser:
-    def __init__(self, client_id:str, client_secret:str, developer_api_base_url:str, access_token_publish_url:str):
+    def __init__(self, client_id:str, client_secret:str, developer_api_base_url:str, access_token_publish_url:str, access_token:str = None):
         """ initialize
         Args:
             client_id (str): CLINED ID of COTOHA API
             client_secret (str): CLIENT SECRET of COTOHA API
             developer_api_base_url (str): Developer API Base URL of COTOHA API
             access_token_publish_url (str): Access Token Publicsh URL of COTOHA API
+            access_token (str): Access token
         """
-        self.client_id = client_id
-        self.client_secret = client_secret
-        self.developer_api_base_url = developer_api_base_url
-        self.access_token_publish_url = access_token_publish_url
-        self.update_access_token()
+        if access_token != None:
+            self.access_token = access_token
+        else:
+            self.client_id = client_id
+            self.client_secret = client_secret
+            self.developer_api_base_url = developer_api_base_url
+            self.access_token_publish_url = access_token_publish_url
+            self.update_access_token()
 
     def update_access_token(self):
         """ update Access Token 
         TODO: use Requests-OAuthlib
         """
+        if self.access_token_publish_url == None:
+            raise RuntimeError("access_token_publish_url is not provided.")
         url = self.access_token_publish_url
         headers={
             "Content-Type": "application/json;charset=UTF-8"
